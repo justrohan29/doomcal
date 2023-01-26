037dd1f44a5e4c436577c87fd3ee1e45ff5b2df6
Rohan Singh <73023108+justrohan29@users.noreply.github.com>
2023-01-26 11:54:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
+from string import ascii_lowercase
 
 encrypt = st.text_input('Enter Text To Encrypt')
-encrypt = encrypt.lower().replace(" ", "")
-
-key = st.slider('Enter Key For Encryption:', 0, 25)
-
-s = ''.join(chr(ord(char) + key) for char in encrypt)
-st.text("Encrypted Text: ")
-st.success(s)
+def caesar_shift(encrypt, places=key):
+    def substitute(char):
+        if char in ascii_lowercase:
+            char_num = ord(char) - 97
+            char = chr((char_num + places) % 26 + 97)
+        return char
+    text = text.lower().replace(' ', '')
+    s = ''.join(substitute(char) for char in encrypt
+    st.text("Encrypted Text: ")
+    st.success(s)
