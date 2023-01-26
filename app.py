bc26c6707d8b168781ab2bf0d52388d472294237
Rohan Singh <73023108+justrohan29@users.noreply.github.com>
2023-01-26 11:55:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def caesar_shift(encrypt, places=key):
             char_num = ord(char) - 97
             char = chr((char_num + places) % 26 + 97)
         return char
-    text = text.lower().replace(' ', '')
+    encrypt = encrypt.lower().replace(' ', '')
     s = ''.join(substitute(char) for char in encrypt
     st.text("Encrypted Text: ")
     st.success(s)
