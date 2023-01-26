ef1be211d43bd27d15cb080db639b994dbc859c9
Rohan Singh <73023108+justrohan29@users.noreply.github.com>
2023-01-26 12:25:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,23 +3,41 @@ import streamlit as st
 sel = st.selectbox('Select:', ['Encryption', 'Decryption'])
 
 if sel=="Encryption":
-    st.text("test1")
-
-text = st.text_input('Enter Text To Encrypt')
-s = st.slider('Choose Key For Encryption:', 0, 25)
-
-def encrypt(text,s):
-    result = ""
- 
-    for i in range(len(text)):
-        char = text[i]
- 
-        if (char.isupper()):
-            result += chr((ord(char) + s-65) % 26 + 65)
- 
-        else:
-            result += chr((ord(char) + s - 97) % 26 + 97)
- 
-    return result
-
-st.success(encrypt(text,s))
+    text = st.text_input('Enter Text To Encrypt')
+    s = st.slider('Choose Key For Encryption:', 0, 25)
+    
+    def encrypt(text,s):
+        result = ""
+     
+        for i in range(len(text)):
+            char = text[i]
+     
+            if (char.isupper()):
+                result += chr((ord(char) + s-65) % 26 + 65)
+     
+            else:
+                result += chr((ord(char) + s - 97) % 26 + 97)
+     
+        return result
+    
+    st.success(encrypt(text,s))
+    
+else:
+    text2 = st.text_input('Enter Text To Encrypt')
+    s2 = st.slider('Choose Key For Encryption:', 0, 25)
+    
+    def encrypt2(text2,s2):
+        result = ""
+     
+        for i in range(len(text2)):
+            char2 = text2[i]
+     
+            if (char2.isupper()):
+                result += chr((ord(char2) - s2-65) % 26 + 65)
+     
+            else:
+                result += chr((ord(char2) - s2 - 97) % 26 + 97)
+     
+        return result
+    
+    st.success(encrypt2(text2,s2))
