74c574e56c576a26716aa861e5dfaef359c6a5a5
Rohan Singh <73023108+justrohan29@users.noreply.github.com>
2023-01-26 11:47:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 encrypt = st.text_input('Enter Text To Encrypt')
 encrypt = encrypt.lower().replace(" ", "")
-s = ''.join(chr(ord(char) + 5) for char in encrypt)
 
+key = st.slider('Enter Key For Encryption:', 0, 25)
+
+s = ''.join(chr(ord(char) + 5) for char in encrypt)
+st.text("Encrypted Text: ")
 st.success(s)
